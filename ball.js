32e22981b955f79514188ba8cbb7b438a939403b
TaleDipanshu <121846979+TaleDipanshu@users.noreply.github.com>
2023-05-06 20:41:08 +0530

    Add files via upload

---
+++ ball.js
@@ -0,0 +1,40 @@
+
+let elementEl=document.getElementById("element-el")
+let count=0
+console.log("hi")
+// elementEl.textContent=12
+function addone(){
+count += 1 
+console.log(count)
+elementEl.innerText=count
+}
+function addtwo(){
+    count += 2 
+    console.log(count)
+    elementEl.innerText=count
+    }
+    function addthree(){
+        count += 3 
+        console.log(count)
+        elementEl.innerText=count
+        }
+
+let elementEl2=document.getElementById("element-el2")
+        let point=0
+        console.log("hi")
+        // elementEl.textContent=12
+        function addonee(){
+        point += 1 
+        console.log(point)
+        elementEl2.innerText=point
+        }
+        function addtwoo(){
+            point += 2 
+            console.log(point)
+            elementEl2.innerText=point
+            }
+            function addthreee(){
+                point += 3 
+                console.log(point)
+                elementEl2.innerText=point
+                }
